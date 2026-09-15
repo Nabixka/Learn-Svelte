b16944e237f0e727f0889ec52f3bcc6443b5f603
nabixka <nabixka05@gmail.com>
2026-09-15 11:44:49 +0700

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+    let password = $state('')
+
+    function generatePassword(){
+        password = ''
+        const abc = "abcdefghijklmnopqrstuvwxyz"
+
+        while(password.length < 12){
+            const randomIndex = Math.round(Math.random() * abc.length)
+            password += abc.charAt(randomIndex)
+        }
+    }
+
+</script>
+
+<div id="buttonGenerate">
+    <h1>Generate Password</h1>
+    <button onclick={generatePassword} id="btn">Click</button>
+</div>
+
+<h3>{password}</h3>
+
+<style>
+    #buttonGenerate{
+        display: flex;
+        gap: 5px;
+    }
+
+    #btn{
+        background-color: gray;
+        color: white;
+    }
+</style>
